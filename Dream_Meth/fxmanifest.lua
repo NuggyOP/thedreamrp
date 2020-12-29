2529db456d3ed7179e5cd9fb1409c0970c7ea3b4
NuggyOP <67390427+NuggyOP@users.noreply.github.com>
2020-12-28 19:53:22 -0500

    uploading meth script to new repo

---
+++ Dream_Meth/fxmanifest.lua
@@ -0,0 +1,15 @@
+-- Resource Metadata
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'Nuggy'
+description 'Simple meth script'
+version '1.0.0'
+
+-- What to run
+client_scripts {
+    'client.lua'
+}
+server_script 'server.lua'
+
+dependency 'bob74_ipl'
